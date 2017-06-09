a04a770b5d6fe5eff11f4e22abd86da053e2eddc
ACE_E303 <ace_e303@e303-s22.lab.lagcc>
2017-06-09 16:45:02 -0400

    close functionality for app

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var userSubmit = document.querySelector('#user-submit');
+var userSubmit = document.querySelector('#user-submit');//These are global variables.
+var toDoList = document.querySelector('#to-do-list');
 
 function addToDo(event){
 	event.preventDefault();
@@ -12,19 +13,36 @@ function addToDo(event){
 		return false;
 	}
 
-	toDoList.innerHTML = '<li>' + userInput.value + '</li>' + toDoList.innerHTML;
+	toDoList.innerHTML = '<li><i class="fa fa-window-close close-to-do" aria-hidden="true"></i>' + userInput.value + '</li>' + toDoList.innerHTML;
 	// toDoList.innerHTML += '<li>' + userInput.value + '</li>' + toDoList.innerHTML ;
 	userInput.value = '';//This refreshed the text box after a word is submitted.
 
-	
 }
+
+	function removeToDo(event){
+		if(event.target.classList.contains('close-to-do')){
+			var li = event.target.parentElement;
+			toDoList.removeChild(li);
+		}
+	}
 
 
 // function toDoList1(){
 // 	this.parentNode.parentNode.removeChild(this.parentNode);
 // }
 
+toDoList.addEventListener('click',removeToDo,false);
+
+userSubmit.addEventListener('click', addToDo),false;
 
 
-userSubmit.addEventListener('click', addToDo);
+
+
+
+
+
+
+
+
+
 
